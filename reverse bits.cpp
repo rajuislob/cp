bc8c491c5ac51b13f8b730490bfb39edc9d883ea
rajuislob <120424506+rajuislob@users.noreply.github.com>
2023-11-06 10:38:30 +0530

    Create reverse bits.cpp

---
+++ reverse bits.cpp
@@ -0,0 +1,8 @@
+uint32_t reverseBits(uint32_t n) {
+        uint32_t rev=0;
+        for(int i=0;i<32;i++){
+            rev=(rev<<1) | (n&1);
+            n=n>>1;
+        }
+        return rev;
+    }
